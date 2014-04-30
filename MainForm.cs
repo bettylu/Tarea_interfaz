3c7b59a3176a002b7e9c3545251e62d54366db63
bettylu <btty.deluna@gmail.com>
2014-04-30 00:18:09 -0500

    interfaz

---
+++ MainForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: betty de luna
+ * Fecha: 29/04/2014
+ * Hora: 12:47 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tarea
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
